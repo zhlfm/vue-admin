62b91d75e7ed9bb8b62e20f12d19ee7e9a84e6df
zhlfm <2926026692@qq.com>
2018-07-28 02:35:28 +0100

    s

--- c.sh
+++ c.sh
@@ -5,7 +5,6 @@ git config user.email '2926026692@qq.com'
 git config credential.helper store
 git remote rm origin
 git remote add origin https://github.com/zhlfm/vue-admin.git
-git pull origin master
 git add .
 git commit -m 's'
 git push origin master
