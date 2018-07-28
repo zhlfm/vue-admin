c5844caa4c48c6275df229ca5547a5485777b98f
zhlfm <2926026692@qq.com>
2018-07-28 10:47:19 +0800

    fg-iview

--- c.sh
+++ c.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 git init
-git config user.name 'zhanghuai'
+git config user.name 'zhlfm'
 git config user.email '2926026692@qq.com'
 git config credential.helper store
-git remote add origin https://gitee.com/ManBuYunDuanZh/vue-admin.git
 git pull origin master
 git add .
 git commit -m 's'
-git push origin master
+git remote add origin https://github.com/zhlfm/fg-admin.git
+git push -u origin master
