5d6c00d6f29098c2a6bd7ca32df4f9698f5576ad
zhlfm <2926026692@qq.com>
2018-07-28 10:23:55 +0800

    admin

--- c.sh
+++ c.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 git init
-git config user.name 'zhlfm'
+git config user.name 'zhanghuai'
 git config user.email '2926026692@qq.com'
 git config credential.helper store
-git remote rm origin
-git remote add origin https://github.com/zhlfm/vue-admin.git
+git remote add origin https://gitee.com/ManBuYunDuanZh/vue-admin.git
+git pull origin master
 git add .
 git commit -m 's'
 git push origin master
